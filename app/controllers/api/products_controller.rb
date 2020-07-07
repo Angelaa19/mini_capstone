9e595766f4abf720953ed082be92162213d94a01
“Angela <angela.anderson.dev@gmail.com>
2020-07-06 20:13:36 -0600

    made model & partial

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,23 +4,36 @@ class Api::ProductsController < ApplicationController
     @product = Product.all
     render 'index.json.jb'
   end
+  
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      image_url: params[:image_url]
+    )
+    @product.save
+    render 'create.json.jb'
+  end
 
   def show
     @product = Product.find_by(id: params[:id])
     render 'show.json.jb'
   end
-  
-  def create
-    @product = Product.new(
-      name: product.name,
-      price: product.price,
-      description: product.description,
-      image_url: product.image_url,
 
-    )
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    @product.image_url = params [:image_url] || @product.image_url
     @product.save
-    
-
+    render 'show.json.jb'
   end
 
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: {message: "Product successfully destroyed."}
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,27 @@
 class Product < ApplicationRecord
+  # def hello
+  #   "say hello"
+  # end
+
+  # def is_discounted?
+  #   if price > 10
+  #     return false
+  #   else 
+  #     return true
+  #   end
+  # end
+
+  def is_discounted?
+    price < 10
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
+
+
 end
